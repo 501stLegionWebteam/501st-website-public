1e9004981152a39f7af57913becd3d80dff7d5bf
witcher112 <witcher112>
2025-09-09 19:11:05 +0000

    deploy from https://github.com/501stLegionWebteam/501st-website/commit/841be19e63fc31b408c51162497beac061fc892a

---
+++ out/_next/static/XFuQbvZoS9-wdnyZ-hyI4/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/d71b8b4174b21a65.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
