0b6eff86c0930429aae740e4e0a2eed98c088460
thekevinbrown <thekevinbrown>
2025-10-16 13:36:36 +0000

    Update site

--- out/_next/static/_5MYBK_hvTWNKEkmMASju/_buildManifest.js
+++ out/_next/static/_5MYBK_hvTWNKEkmMASju/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/005f34c5ff3fbe01.js"
+    "./static/chunks/5dec0a2d14286999.js"
   ],
   "__rewrites": {
     "afterFiles": [],
--- out/_next/static/chunks/005f34c5ff3fbe01.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/5b8f77abfd4b4980.js",
-  "static/chunks/7dff330228ed6753.js",
-  "static/chunks/turbopack-4823465137af8ba3.js"
-])
